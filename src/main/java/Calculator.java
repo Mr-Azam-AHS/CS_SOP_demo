69d283a763c7eef09f8d5b2b42d9c50028d9a6c1
Jack-Of-All <jackofalllearning@gmail.com>
2026-02-01 20:46:07 -0600

    Changed demo code

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+   public int add(int a, int b){
+    //TODO: Implement this method
+    return 0;
+   } 
+
+   public static void main(String[] args) {
+       //Use this method to test manually
+
+       System.out.println("Run the tests to check your work!");
+   }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    
+    @Test
+    public void testAddition(){
+        Calculator calc = new Calculator();
+
+        assertEquals(5, calc.add(2,3), "Addition failed for input 2 + 3");
+    }
+}
